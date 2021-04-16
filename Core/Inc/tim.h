a8afca9d43568820daf0f40c6627936eef2b8e8b
cuteWarmFrog <livshits.victor@gmail.com>
2021-04-16 15:08:40 +0300

    blinking lights, trying to make some sound

---
+++ Core/Inc/tim.h
@@ -0,0 +1,48 @@
+/*
+ * tim.h
+ *
+ *  Created on: Apr 16, 2021
+ *      Author: livshits
+ */
+
+#ifndef __tim_H
+#define __tim_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+extern TIM_HandleTypeDef htim2;
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+void MX_TIM2_Init(void);
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ tim_H */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
